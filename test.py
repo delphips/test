a86c3cf177c474de60393610f88e0c80a1862bc9
ZhangYongQiang <delphips@126.com>
2023-10-16 22:13:04 +0800

    init

---
+++ test.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+#print(tf.__version__)
+#print(tf.test.gpu_device_name())
+
+config = tf.compat.v1.ConfigProto()
+print(config.list_physical_devices('GPU'))
+ 
+a = tf.constant(2.)
+b = tf.constant(4.)
+ 
+print(a * b)
